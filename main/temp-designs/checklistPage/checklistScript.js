3dd9eec0590cb4190e981364c7a9ac9a2dc09e7e
ethanmmhaugen <105734126+ethanmmhaugen@users.noreply.github.com>
2023-07-15 18:11:10 -0500

    Completes pages toggling using Ids

--- main/temp-designs/checklistPage/checklistScript.js
+++ main/temp-designs/checklistPage/checklistScript.js
@@ -1,11 +1,15 @@
-const visiblePage = 'section-1'
-const pages = ['section-1','section-2','section-3'];
+const LOCAL_STORAGE_VISIBLE_PAGE = 'section-1'
+let visiblePage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_VISIBLE_PAGE)) || 'section-1'
 
-document.getElementById('section-1-button').addEventListener("click", function(){(toggle_visibility('section-2'))})
+document.getElementById('section-1-next-button').addEventListener("click", function(){(toggle_visibility('section-2', visiblePage))})
+document.getElementById('section-2-prev-button').addEventListener("click", function(){(toggle_visibility('section-1', visiblePage))})
+document.getElementById('section-2-next-button').addEventListener("click", function(){(toggle_visibility('section-3', visiblePage))})
+document.getElementById('section-3-prev-button').addEventListener("click", function(){(toggle_visibility('section-2', visiblePage))})
+document.getElementById('section-3-finish-button').addEventListener("click", function(){})
 
-function toggle_visibility(id) {
+function toggle_visibility(id, oldPage) {
     console.log('made it')
-    var old_e = document.getElementById(visiblePage);
+    var old_e = document.getElementById(oldPage);
     var new_e = document.getElementById(id);
     if(old_e) {
         console.log('old', old_e, 'none');
@@ -13,6 +17,12 @@ function toggle_visibility(id) {
     }
     console.log('new', new_e, 'block');
     new_e.style.display = 'grid';   
-    top.visible_div_id = id; 
-         
+    visiblePage = id; 
+    save()     
 }
+
+function save(){
+    localStorage.setItem(LOCAL_STORAGE_VISIBLE_PAGE, JSON.stringify(visiblePage))
+}
+
+toggle_visibility(visiblePage, 'section-1');
